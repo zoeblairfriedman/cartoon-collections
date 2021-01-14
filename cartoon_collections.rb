1dd949dd42e91ac69a26df59564d33963bb14023
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-14 09:16:51 -0500

    commit tree ab659bfb0a74a819cd1e1e52f8f1bfeb96542b18

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+  array.each_with_index do |dwarf, index|
+    puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# seven = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+# roll_call_dwarves(seven)
+
+def summon_captain_planet(elements)
+  elements.map { |i| i.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+# summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(calls)
+  calls.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# short_words = ["two", "go", "industrious", "bop"]
+# long_planeteer_calls(short_words)
+
+def find_the_cheese(string_array)
+  result = ""
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # iterate over the string_array
+  string_array.each do |ele|
+    # iterate over the cheese array
+    cheese_types.each do |cheese|
+      #if the cheese exits, return the cheese
+      if cheese == ele
+        result = cheese
+      end
+    end
+  end
+  if result.length == 0
+    return
+    nil
+  else
+  return result
+  end
 end
+
